ab8bf55c6b435d8a9e0c3714b2decee4a9e90b00
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-10-14 19:38:18 +0530

    test: Add script to create pull requests

---
+++ .github/pull-req.py
@@ -0,0 +1,10 @@
+import os, sys, requests
+
+token = os.environ['TOKEN']
+branch = str(sys.argv[1])
+
+header = {'Authorization': 'token ' + token, 'Accept': 'application/vnd.github.v3+json'}
+url = "https://api.github.com/repos/linkhub-org/linkhub-deployment/pulls/"
+data = {'title': branch.capitalize() + " Deployment", 'head': branch, base: 'main'}
+
+response = requests.post(url, headers=header, data=data)
