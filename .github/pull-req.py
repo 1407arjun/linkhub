29c470d1fa6a419fc993f45223a45004152a0be9
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-10-14 19:45:29 +0530

    fix: Syntax errors in Python script file

--- .github/pull-req.py
+++ .github/pull-req.py
@@ -5,6 +5,6 @@ branch = str(sys.argv[1])
 
 header = {'Authorization': 'token ' + token, 'Accept': 'application/vnd.github.v3+json'}
 url = "https://api.github.com/repos/linkhub-org/linkhub-deployment/pulls/"
-data = {'title': branch.capitalize() + " Deployment", 'head': branch, base: 'main'}
+data = {'title': branch.capitalize() + " Deployment", 'head': branch, 'base': 'main'}
 
 response = requests.post(url, headers=header, data=data)
