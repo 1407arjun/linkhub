a1942aaa9cd454e86f187137bfada0b58f5d5424
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-10-14 20:33:33 +0530

    Update pull-req.py

--- .github/pull-req.py
+++ .github/pull-req.py
@@ -6,7 +6,7 @@ print(branch)
 
 header = {'Authorization': 'token ' + token, 'Accept': 'application/vnd.github.v3+json'}
 url = "https://api.github.com/repos/linkhub-org/linkhub-deployment/pulls"
-data = {'title': branch.capitalize() + " Deployment", 'head': branch, 'base': 'main'}
+data = {'title': 'Deployment', 'head': branch, 'base': 'main'}
 
 response = requests.post(url, headers=header, data=data)
 print(response.json())
