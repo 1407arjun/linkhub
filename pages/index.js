b02b11adf789c8e25dfd9b549e98771fd384ee95
Arjun <arjun140702@gmail.com>
2021-10-15 14:37:34 +0000

    chore: Update frontend deployment

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+       <Head>
+        <title>LinkHub</title>
+        <meta name="description" content="An open source resources network" />
+        <link rel="icon" href="/favicon.ico" />
+       </Head>
        <Image src="/comingsoon.png" alt="Coming Soon" layout="responsive" />
     </div>
   )
