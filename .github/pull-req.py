e19489267468228f1e54f0d4d5fa860a82125c2e
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-10-15 19:29:50 +0530

    chore: Update head and base branch

--- .github/pull-req.py
+++ .github/pull-req.py
@@ -1,10 +1,9 @@
 import os, sys, requests
 
 token = os.environ['TOKEN']
-branch = str(sys.argv[1])
 
 header = {'Authorization': 'token ' + token, 'Accept': 'application/vnd.github.v3+json'}
 url = "https://api.github.com/repos/linkhub-org/linkhub-deployment/pulls"
-data = {'title': branch.capitalize() + ' Deployment', 'head': branch, 'base': 'dev'}
+data = {'title': branch.capitalize() + ' Deployment', 'head': 'dev', 'base': 'main'}
 
 response = requests.post(url, headers=header, json=data)
