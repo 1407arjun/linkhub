27eec48a5f874c686ea0b66c6cfa91eef5a231f0
Arjun <arjun140702@gmail.com>
2021-10-25 15:29:08 +0000

    chore: Update frontend deployment

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
-       <Head>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>LinkHub</title>
-        <meta name="description" content="An open source resources network" />
-        <link rel="icon" href="/favicon.ico" />
-       </Head>
-       <Image src="/comingsoon.png" alt="Coming Soon" layout="responsive" />
+        <meta name='description' content='An open source resources network' />
+        <link rel='icon' href='/assets/favicon.ico' />
+      </Head>
+      <Image src='/comingsoon.png' alt='Coming Soon' className="img-fluid" width="120"/>
     </div>
   )
 }
