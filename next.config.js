b748425eed719b7ab37b1d243710885e5340c1f6
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-11-01 12:40:57 +0530

    Remove redirect to landing page

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/landing',
-        permanent: false
-      }
-    ]
-  }  
+  } 
 }
