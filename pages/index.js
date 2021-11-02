7709feae3d17bf3aeed0809d5917eba8d6e08d08
Arjun <arjun140702@gmail.com>
2021-11-02 12:55:04 +0530

    Fix issues with responsiveness

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ export default function Index() {
     <div className="flex flex-col place-content-start h-full">
       <Head title="An open source resources network &middot; LinkHub"/>
       <div className="flex flex-col flex-auto place-content-center w-full bg-cover bg-home-1 h-full p-4">
-        <div className="flex flex-row px-4 py-2">
-          <img src="/assets/logo.svg" className="w-1/2 sm:w-1/3 lg:w-1/4 xl:w-1/6 mx-auto lg:mr-4" />
-          <div className="hidden lg:inline-flex flex-row flex-auto place-content-center lg:justify-end lg:content-center lg:mr-4">
+        <div className="flex flex-row px-4 py-4">
+          <img src="/assets/logo.svg" className="w-1/2 sm:w-1/3 md:w-1/4 xl:w-1/6 mx-auto lg:mr-4" />
+          <div className="hidden md:inline-flex flex-row flex-auto place-content-center md:justify-end md:content-center md:mr-4">
             <h1>Hello</h1>
             <h1>Hello</h1>
             <h1>Hello</h1>
@@ -15,12 +15,12 @@ export default function Index() {
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row flex-auto place-content-center w-full">
-          <div className="flex flex-col flex-auto place-content-center p-16 w-full gap-y-2 text-center md:justify-center md:content-start md:text-left w-1/2">
-            <h1 className="text-white text-6xl font-bold">An open source resources network</h1>
-            <h2 className="text-white text-3xl text-opacity-40">Insert multiline tagline here</h2>
+        <div className="flex flex-col md:flex-row flex-auto place-content-center w-full text-center md:text-left">
+          <div className="flex flex-col flex-auto place-content-center p-8 w-full gap-y-2 md:justify-center md:content-start w-full md:w-2/3 lg:w-1/2">
+            <h1 className="text-white text-5xl lg:text-6xl font-bold">An open source resources network</h1>
+            <h2 className="text-white text-xl lg:text-3xl text-opacity-40">Insert multiline tagline here</h2>
           </div>
-          <img src="/assets/home-2.gif" className="m-16 md:m-8 object-contain"/>
+          <img src="/assets/home-2.gif" className="mt-4 mb-8 mx-auto md:m-4 object-contain w-3/4 sm:w-2/3 md:w-1/3 lg:w-1/2"/>
         </div>
       </div>
     </div>
