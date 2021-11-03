bc15cd7ad70eef28052b163f4c635d637e31dafd
Arjun <arjun140702@gmail.com>
2021-11-03 12:22:10 +0530

    Fix bugs in responsiveness

--- pages/components/index/signupbar.jsx
+++ pages/components/index/signupbar.jsx
@@ -1,12 +1,12 @@
 export default function SignUpBar() {
     return (
         <form action="/signup" method="POST" className="mt-4 md:mt-8">
-            <div className="flex flex-col sm:flex-row justify-items-strech sm:justify-items-start gap-y-2 sm:gap-x-2 sm:mx-auto">
+            <div className="flex flex-col md:flex-row justify-items-strech md:justify-items-start gap-y-2 md:gap-x-2 md:mx-auto">
                 <input type="text" className="self-center rounded-md bg-white px-4 py-3 shadow-lg placeholder-gray-500
-                text-m sm:text-l lg:text-xl w-full sm:w-3/5 focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-60" 
+                text-m sm:text-l lg:text-xl w-full md:w-2/3 lg:w-3/5 focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-60" 
                 placeholder="Email address"/>
-                <button type="submit" className="self-center bg-blue-500 px-2 py-3 rounded-md
-                text-m sm:text-l lg:text-xl font-bold text-white shadow-lg w-full sm:w-2/5 focus:outline-none focus:ring focus:ring-opacity-60">Sign up for LinkHub</button>
+                <button type="submit" className="self-center bg-blue-600 px-2 py-3 rounded-md
+                text-m sm:text-l lg:text-xl font-bold text-white shadow-lg w-full md:w-1/3 lg:w-2/5 focus:outline-none focus:ring focus:ring-opacity-60">Sign up for LinkHub</button>
             </div>
         </form>
     )
--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,14 @@ export default function Index() {
       <Head title="LinkHub &middot; An open source resources network"/>
       <div className="wrapper grad-home p-4"> {/* bg-cover bg-home-1*/}
         <HomeBar />
-        <div className="mx-auto my-4 md:my-8 flex flex-col-reverse md:flex-row place-content-center w-full text-center md:text-left">
-          <div className="flex flex-col flex-grow-1 place-content-center p-6 md:p-8 w-full gap-y-2 md:justify-center md:content-start w-full md:w-2/3 lg:w-1/2">
-            <h1 className="md:mt-8 text-white text-4xl sm:text-5xl lg:text-6xl font-bold">An open source resources network</h1>
+        <div className="mx-auto my-4 lg:my-8 flex flex-col-reverse lg:flex-row place-content-center w-full text-center lg:text-left">
+          <div className="flex flex-col flex-grow-1 place-content-center p-6 lg:p-8 w-full gap-y-2 lg:justify-center lg:content-start w-full lg:w-1/2">
+            <h1 className="lg:mt-8 text-white text-4xl sm:text-5xl lg:text-6xl font-bold">An open source resources network</h1>
             <h2 className="text-white text-m sm:text-l lg:text-2xl text-opacity-60">{ tagline }</h2>
             <SignUpBar />
           </div>
 
-          <img src="/assets/home-2.gif" className="mt-4 mb-8 mx-auto md:m-4 object-contain w-3/4 sm:w-2/3 md:w-1/3 lg:w-1/2" alt=""/>
+          <img src="/assets/home-2.gif" className="mt-4 mb-8 mx-auto lg:m-4 object-contain w-3/4 sm:w-2/3 lg:w-1/2" alt=""/>
         </div>
       </div>
     </div>
