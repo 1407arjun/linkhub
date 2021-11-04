df1b771dbfc4810ddbd8241d16f30ef3820b24dc
Arjun <arjun140702@gmail.com>
2021-11-04 15:29:23 +0530

    Make navbar responsive

--- components/home/navbar.jsx
+++ components/home/navbar.jsx
@@ -2,7 +2,7 @@ import NavBtn from "./navbtn"
 
 export default function NavBar(props) {
     return (
-        <div className="hidden md:flex flex-col place-content-center py-8 gap-8 border-r border-gray-300">
+        <div className="hidden md:flex flex-col place-content-center py-2 gap-2 border-r border-gray-300">
             <NavBtn src="/assets/icon-black.svg" alt="" current={ false }/>
             <NavBtn src="/assets/home/grid.svg" alt="Home" current={ props.current === "Home"}/>
             <NavBtn src="/assets/home/stars.svg" alt="Explore" current={ props.current === "Explore"}/>
