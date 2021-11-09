c3754bd6d47f61642aa48f188817d907b6448560
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-11-10 00:56:13 +0530

    Update tab names

--- components/profile/tablayout.jsx
+++ components/profile/tablayout.jsx
@@ -2,8 +2,8 @@ export default function TabLayout() {
     return (
         <div className="overflow-x-auto grid grid-cols-3 justify-evenly items-center w-full gap-2 sm:gap-4 text-center font-semibold text-base md:text-lg">
             <button className="font-semibold py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 border-b-2 border-blue-500 dark:text-white">Your Posts</button>
-            <button className="py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 dark:text-white">Upvoted Posts</button>
-            <button className="py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 dark:text-white">Downvoted Posts</button>
+            <button className="py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 dark:text-white">Upvoted</button>
+            <button className="py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 dark:text-white">Downvoted</button>
         </div>
     )
 }
