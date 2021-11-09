f2df9f6fc986caef077fb134cf7883abfc400010
Arjun <arjun140702@gmail.com>
2021-11-09 16:24:02 +0530

    Add react-markdown for preview

--- components/editor/preview.jsx
+++ components/editor/preview.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import context from './context'
+import ReactMarkdown from 'react-markdown'
 
 export default function Preview() {
     const { markdownText } = useContext(context)
@@ -7,6 +8,7 @@ export default function Preview() {
     return (
         <div className="flex flex-col justify-start items-center gap-2">
             <h4 className="w-full text-left text-base md:text-lg xl:text-xl font-semibold mb-1">Preview</h4>
+            <ReactMarkdown source={ markdownText }/>
         </div>
     )
 }
