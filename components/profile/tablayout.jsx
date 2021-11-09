8a23a26e5e67cfeb82136a93966f0301f623ff48
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-11-10 00:53:42 +0530

    Update text sizes

--- components/profile/tablayout.jsx
+++ components/profile/tablayout.jsx
@@ -1,6 +1,6 @@
 export default function TabLayout() {
     return (
-        <div className="overflow-x-auto grid grid-cols-3 justify-evenly items-center w-full gap-2 sm:gap-4 text-center font-semibold text-xs sm:text-base md:text-lg">
+        <div className="overflow-x-auto grid grid-cols-3 justify-evenly items-center w-full gap-2 sm:gap-4 text-center font-semibold text-base md:text-lg">
             <button className="font-semibold py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 border-b-2 border-blue-500 dark:text-white">Your Posts</button>
             <button className="py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 dark:text-white">Upvoted Posts</button>
             <button className="py-1 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-gray-800 dark:focus:bg-gray-800 dark:text-white">Downvoted Posts</button>
