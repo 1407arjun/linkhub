f1502826d46894ad2c540fe362204415b7d049b8
Arjun <arjun140702@gmail.com>
2021-11-10 22:35:46 +0530

    Refactor structure, add fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css'
-import '../public/styles/globals.css'
+import '../../public/styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
     extend: {
