ca20479960895ed0ca7ef18773fc37a3c865bd04
Arjun <76874556+1407arjun@users.noreply.github.com>
2021-11-11 22:42:06 +0530

    Implement dynamic routing

--- src/pages/profile/[profile].js
+++ src/pages/profile/[profile].js
@@ -1,6 +1,10 @@
-import View from "../../components/views/view"
+import View from '../../components/views/view'
+import { useRouter } from 'next/router'
 
 export default function Profile() {
+    const router = useRouter()
+    const { profile } = router.query
+
     return (
         <View name="Arjun" desc="Hello" add={ false } img="/assets/profile/person.svg" head="Profile on LinkHub"/>
     )
