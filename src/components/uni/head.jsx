b6f49bfc632e1311339442c2337cbf1070a43fca
Arjun <arjun140702@gmail.com>
2021-11-21 19:59:14 +0530

    Add dynamic favicon

--- src/components/uni/head.jsx
+++ src/components/uni/head.jsx
@@ -1,13 +1,24 @@
 import NextHead from 'next/head'
+import { useState, useEffect } from 'react'
 
 export default function Head(props) {
+    const [theme, setTheme] = useState()
+
+    useEffect(() => {
+        setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+            setTheme(e.matches ? "dark" : "light")
+        });
+    }, [theme])
+
+
     return (
       <NextHead>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <title>{ props.title }</title>
         <meta name='description' content="An open source resources network"/>
-        <link rel='icon' href='/assets/favicon.ico' />
+        <link rel='icon' href={ theme === "dark" ? '/assets/icon.svg' : '/assets/icon-black.svg'}/>
       </NextHead>
     )
   }
