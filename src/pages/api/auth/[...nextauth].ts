6c92544502534eaf2797aac4ebda81a5dab9892f
Arjun <arjun140702@gmail.com>
2021-11-30 12:49:11 +0530

    Add sample code for GitHub OAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import dotenv from 'dotenv'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from 'next-auth/providers/github'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import client from '../../../server/loaders/database'
+import axios from 'axios'
 
 dotenv.config()
 
@@ -15,6 +17,12 @@ export default async function Auth(req: NextApiRequest, res: NextApiResponse) {
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
                 profileUrl: "https://www.googleapis.com/oauth2/v3/userinfo"
             })
+            //error=OAuthAccountNotLinked
+            /*GitHubProvider({
+                clientId: process.env.GITHUB_CLIENT_ID!,
+                clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+                profileUrl: "https://api.github.com/user",    
+            })*/
         ],
         adapter: MongoDBAdapter(client),
         secret: process.env.AUTH_SECRET!,
@@ -24,6 +32,19 @@ export default async function Auth(req: NextApiRequest, res: NextApiResponse) {
         },
         callbacks: {
             async signIn({ user, account, profile, email, credentials }) { 
+                /*console.log(account)
+                console.log(user)
+                console.log(profile)
+                if (account.provider === "github") {
+                    const token = account.access_token
+                    const res = await axios.get("https://api.github.com/user/emails", {
+                        headers: {
+                            'Authorization': ("token " + token),
+                            'Accept': 'application/vnd.github.v3+json'
+                        }
+                    })
+                    console.log(res)
+                }*/
                 return true
             }
         }
