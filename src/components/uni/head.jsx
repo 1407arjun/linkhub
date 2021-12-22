45f611dae71be77905299f61495feea5bd2a788d
Arjun <arjun140702@gmail.com>
2021-12-22 16:59:57 +0530

    Add Google site verification

--- src/components/uni/head.jsx
+++ src/components/uni/head.jsx
@@ -1,8 +1,5 @@
 import NextHead from 'next/head'
 import { useState, useEffect } from 'react'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 export default function Head(props) {
     const [theme, setTheme] = useState()
@@ -17,7 +14,7 @@ export default function Head(props) {
 
     return (
       <NextHead>
-        <meta name="google-site-verification" content={ process.env.GOOGLE_SITE_VERIFICATION } />
+        <meta name="google-site-verification" content="8II1ZS008rD9Vb8kkQlv8ipbKgfhLrjKDf-zj07RvsI" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <title>{ props.title }</title>
