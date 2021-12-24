f138997afd0239da5d53006c44b0725aa0d11551
Arjun <arjun140702@gmail.com>
2021-12-24 12:11:59 +0530

    Update <a> to <Link>

--- src/components/login/user.jsx
+++ src/components/login/user.jsx
@@ -16,7 +16,7 @@ export default function User(props) {
                     <h1 className="text-black dark:text-white text-5xl font-bold mb-6">{ props.line1 }</h1>
                     <h2 className="text-black dark:text-white text-4xl font-bold mb-4">{ props.line2 }</h2>
                     <SignOpts type={ props.type } href={ props.loc }/>
-                    <p className="dark:text-white">{ props.rdesc }<span><a href={ props.rloc } className="text-blue-500 hover:underline focus:underline">{ props.rtype }</a></span></p>
+                    <p className="dark:text-white">{ props.rdesc }<span><Link href={ props.rloc }><a className="text-blue-500 hover:underline focus:underline">{ props.rtype }</a></Link></span></p>
                     <p className="font-bold dark:text-white">OR</p>
                     <p className="dark:text-white"><span><Link href="/explore"><a className="text-blue-500 hover:underline focus:underline">Explore LinkHub</a></Link></span> without an account</p>
                 </div>
