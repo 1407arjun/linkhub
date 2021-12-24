c16109460592789687ed49487234eeefac78a774
Arjun <arjun140702@gmail.com>
2021-12-24 12:00:33 +0530

    Add a temporary redirect

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false
+      }
+    ]
   } 
 }
