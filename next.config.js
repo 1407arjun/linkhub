ded6c506fb5069401828828cf530946ed4ffd222
Arjun Sivaraman <76874556+1407arjun@users.noreply.github.com>
2022-01-05 13:11:24 +0530

    Remove redirects

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: false
-      }
-    ]
-  } 
+  }
 }
