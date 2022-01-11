30f3f8db346a5c4852b8ff01b5b565ca6ba55513
Arjun Sivaraman <76874556+1407arjun@users.noreply.github.com>
2022-01-11 14:54:26 +0530

    Remove obsolete workflows

--- .github/pull-req.py
+++
@@ -1,9 +0,0 @@
-import os, sys, requests
-
-token = os.environ['TOKEN']
-
-header = {'Authorization': 'token ' + token, 'Accept': 'application/vnd.github.v3+json'}
-url = "https://api.github.com/repos/linkhub-org/linkhub-deployment/pulls"
-data = {'title': 'Push code to production', 'head': 'dev', 'base': 'main'}
-
-response = requests.post(url, headers=header, json=data)
