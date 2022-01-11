ba5e780ec3a55b92b22aa30094238e09f36217e1
Arjun <arjun140702@gmail.com>
2022-01-11 20:24:17 +0530

    Improve landing page

---
+++ src/components/index/quicksearch.jsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router"
+
+export default function QuickSearch() {
+    const router = useRouter()
+
+    async function postQuery(ev) {
+        ev.preventDefault()
+        router.push("/explore?query=" + ev.target[0].value)
+    }
+
+    return (
+        <form onSubmit={ postQuery } className="mt-4 md:mt-8">
+            <div className="flex flex-col md:flex-row justify-items-strech md:justify-items-start gap-y-2 md:gap-x-2 md:mx-auto">
+                <input type="text" className="self-center rounded-md bg-white bg-opacity-20 backdrop-blur-sm px-4 py-3 shadow-lg placeholder-gray-100
+                text-base sm:text-lg lg:text-xl w-full md:w-2/3 lg:w-2/3 focus:outline-none ring-1 ring-white ring-opacity-60 focus:ring-opacity-100" 
+                placeholder="Quick search..." autoComplete="off"/>
+                <button type="submit" className="self-center bg-blue-600 px-2 md:px-1 xl:px-2 py-3 rounded-md
+                text-base sm:text-lg lg:text-xl font-bold text-white shadow-lg w-full md:w-1/3 lg:w-1/3 focus:outline-none focus:ring focus:ring-opacity-60">Get started</button>
+            </div>
+        </form>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from '../components/uni/head'
 import Footer from '../components/index/footer'
 import HomeBar from '../components/index/homebar'
-import SignUpBar from '../components/index/signupbar'
+import QuickSearch from '../components/index/quicksearch'
 import { getSession } from 'next-auth/react'
 
 const tagline = "A portal to help you find the right resources to boost your learning from all those available on the World Wide Web. Links are ranked based on the feedback by others who have tried it out, so as to provide you only the best of the best."
@@ -12,11 +12,11 @@ export default function Index() {
       <Head title="LinkHub &middot; An open source resources network" desc="LinkHub Homepage"/>
       <div className="grad-home min-h-screen">
         <HomeBar/>
-        <div className="mx-auto my-4 flex flex-col-reverse lg:flex-row place-content-center w-full text-center lg:text-left px-4">
+        <div className="mx-auto my-6 flex flex-col-reverse lg:flex-row place-content-center w-full text-center lg:text-left px-4">
           <div className="flex flex-col grow-1 place-content-center p-6 lg:p-8 gap-y-2 lg:justify-center lg:content-start w-full lg:w-2/3 xl:w-1/2">
             <h1 className="lg:mt-8 text-white text-4xl sm:text-5xl lg:text-6xl font-bold">An open source resources network</h1>
             <h2 className="text-white font-normal text-base sm:text-lg lg:text-2xl text-opacity-60">{ tagline }</h2>
-            <SignUpBar/>
+            <QuickSearch/>
           </div>
           <img src="/assets/home-2.gif" className="object-contain mt-4 mb-8 mx-auto lg:m-4 w-3/4 sm:w-2/3 lg:w-1/2" alt=""/>
         </div>
